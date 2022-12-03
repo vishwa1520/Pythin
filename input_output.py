053d0b42ea277c7e133560da5b0ccbb329599f15
vishwa1520 <119159764+vishwa1520@users.noreply.github.com>
2022-12-03 23:34:17 +0530

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -1,12 +1,55 @@
 # Python
-print(""hello world)
-# Python
-print("hello world")
-print(" Hi vishwa")
-# Python
 #print("hello world")
 #print(" Hi vishwa")
-int_var=10
+#type and datatype
+int_var=20
 print("int value ",int_var)
 print("check datatpye of functions",type(int_var))
 #commit
+
+float_var=15.5
+print("int value ",float_var)
+print("check datatpye of functions",type(float_var))
+
+bool_var=True
+print("int value ",bool_var)
+print("check datatpye of functions",type(bool_var))
+
+
+str_var='i am vishwa'
+print("int value ",str_var)
+print("check datatpye of functions",type(str_var))
+
+#casting
+int_var=int_var+10
+cast_int_var=float(int_var)
+cast_float_var=int(float_var)
+print("casteed method ",cast_int_var)
+print("casteed method ",cast_float_var)
+#print("casteed method ",int_var)
+
+
+
+#numeric string
+
+num_str= int("125")+ float(50)
+print("numeric str",num_str)
+
+
+#input functions
+
+#name=input()
+#age=input()
+#print("user name = ",name)
+#print("user age =",age)
+
+name=input("user name")
+age=input("user age")
+
+print("name of user = ",type(name))
+print("age of user = ",age)
+
+
+
+
+
