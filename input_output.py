a2e9eb637b1edc6473cdbecdddbe281cecab53d7
vishwa1520 <119159764+vishwa1520@users.noreply.github.com>
2022-12-03 22:36:50 +0530

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -3,3 +3,10 @@ print(""hello world)
 # Python
 print("hello world")
 print(" Hi vishwa")
+# Python
+#print("hello world")
+#print(" Hi vishwa")
+int_var=10
+print("int value ",int_var)
+print("check datatpye of functions",type(int_var))
+#commit
