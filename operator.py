96dc1543f073ca20f3df80eeb48f9c0e7c15c3f2
vishwa1520 <119159764+vishwa1520@users.noreply.github.com>
2022-12-04 12:56:54 +0530

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,54 @@
+# numeric operator
+
+x=5
+y=2
+
+print(" addtion =",x+y)
+print(" multi = ",x*y)
+print(" subs = ",x-y)
+print(" float divi = ",x/y)
+print(" int div = ",x//y)
+print(" modulus   = ",x%y)
+print(" power of cal   = ",x**y)
+
+
+# concat operation for string
+
+str_data="vishwa"
+
+full_name=str_data+" "+ "wase"
+print("full name = ",full_name)
+
+#minus_substr="vishwa"-"wase"
+#print("subtr name",minus_substr)
+
+pow_str="vishwa" * 3
+print("pow str =",pow_str)
+
+
+# comparison operator
+
+a=5
+b=10
+
+ 
+print("eual to ",a==b)
+print("eual to ",a!=b)
+print("eual to ",a>b)
+print("eual to ",a<b)
+print("eual to ",a>=b)
+print("eual to ",a<=b)
+
+
+
+# logical operator
+
+m=5
+n=8
+
+print ("and operation ",m>10 and n<10)
+print ("or operation ",m>10 or n<9)
+print ("and operation ",not(m>m and n<10) )
+
+
+
