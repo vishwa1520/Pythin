ff517229f68cac0cf3f5cebb973e6508ae54ad88
vishwa1520 <119159764+vishwa1520@users.noreply.github.com>
2022-12-04 20:00:11 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,14 @@
+x=50
+if x==50:
+    print("yes, x equl to =" ) 
+    
+marks=50
+
+if marks>=90:
+    print("grade a")
+elif marks>=80 and marks<70:
+    print("grade b")
+elif marks>=50 and marks<60:
+     print("grade c")
+else:
+    print("c")
